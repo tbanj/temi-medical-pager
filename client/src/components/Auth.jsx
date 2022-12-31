9ae30c105abb86a572286fbb66bbd6bd70198ee5
Alabi Temitope <t.banji@rocketmail.com>
2022-12-31 16:33:30 +0100

    updated

---
+++ client/src/components/Auth.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import Cookies from "universal-cookie";
+import axios from "axios";
+
+import signinImage from "../assets/signup.jpg";
+
+const Auth = () => {
+  const [isSignUP, setIsSignUP] = useState(false);
+  return (
+    <div className="auth__form-container">
+      <div className="auth__form-container_fields">
+        <div className="auth__form-container_fields-content">
+          <p>{isSignUP ? "Sign Up" : "Sign In"}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
