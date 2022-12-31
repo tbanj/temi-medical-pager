755b4cf57d8979ae811672eb9c4bdcf2920b5605
Alabi Temitope <t.banji@rocketmail.com>
2022-12-31 18:26:23 +0100

    updated

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -5,12 +5,97 @@ import axios from "axios";
 import signinImage from "../assets/signup.jpg";
 
 const Auth = () => {
-  const [isSignUP, setIsSignUP] = useState(false);
+  const [isSignUP, setIsSignUP] = useState(true);
+
+  const switchMode = () => {
+    setIsSignUP((prevIsSignUp) => !prevIsSignUp);
+  };
+  const handleChange = () => {};
+
   return (
     <div className="auth__form-container">
       <div className="auth__form-container_fields">
         <div className="auth__form-container_fields-content">
           <p>{isSignUP ? "Sign Up" : "Sign In"}</p>
+          <form onSubmit={() => {}}>
+            {isSignUP && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="fullName">Full Name</label>
+                <input
+                  name="fullName"
+                  placeholder="Full Name"
+                  onChange={handleChange}
+                  required
+                  type="text"
+                />
+              </div>
+            )}
+            <div className="auth__form-container_fields-content_input">
+              <label htmlFor="username"> UserName</label>
+              <input
+                name="userName"
+                placeholder="UserName"
+                onChange={handleChange}
+                required
+                type="text"
+              />
+            </div>
+            {isSignUP && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="phoneNumber">Phone Number</label>
+                <input
+                  name="phoneNumber"
+                  placeholder="Phone Number"
+                  onChange={handleChange}
+                  required
+                  type="text"
+                />
+              </div>
+            )}
+            {isSignUP && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="avatarURL">Avatar URL</label>
+                <input
+                  name="avatarURL"
+                  placeholder="Avatar URL"
+                  onChange={handleChange}
+                  required
+                  type="text"
+                />
+              </div>
+            )}
+
+            <div className="auth__form-container_fields-content_input">
+              <label htmlFor="password">Password</label>
+              <input
+                name="password"
+                placeholder="Password"
+                onChange={handleChange}
+                required
+                type="password"
+              />
+            </div>
+            {isSignUP && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input
+                  name="confirmPassword"
+                  placeholder="Confirm Password"
+                  onChange={handleChange}
+                  required
+                  type="password"
+                />
+              </div>
+            )}
+          </form>
+          <div className="auth__form-container_fields-account">
+            <p>
+              {isSignUP ? "Already have an account?" : "Don't have an account"}
+              <span onClick={switchMode}>
+                {isSignUP ? "Sign In" : "Sign Up"}
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
